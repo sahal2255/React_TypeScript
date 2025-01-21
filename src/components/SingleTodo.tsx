b1b3e93284dd285860a8e212a0658d0c34023ca5
sahal2255 <sahalvv9656@gmail.com>
2025-01-21 19:35:32 +0530

    delete

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Todo } from "../model";
+import { FiEdit3 } from "react-icons/fi"; // Edit icon
+import { MdDelete } from "react-icons/md"; // Delete icon
 
-const SingleTodo:React.FC = () => {
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  // Handle delete function
+  const handleDelete = () => {
+    setTodos(todos.filter((t) => t.id !== todo.id));
+  };
+
+  // Handle toggle completion
+  const handleToggleComplete = () => {
+    setTodos(
+      todos.map((t) =>
+        t.id === todo.id ? { ...t, isCompleted: !t.isCompleted } : t
+      )
+    );
+  };
+
   return (
-    <div>
-      
+    <div className="bg-white rounded-lg shadow-md p-4 flex items-center justify-between w-64">
+      <span
+        className={`text-lg font-medium ${
+          todo.isCompleted ? "line-through text-gray-500" : "text-gray-800"
+        }`}
+      >
+        {todo.name}
+      </span>
+      <div className="flex gap-2">
+        <button
+          className="text-blue-500 hover:text-blue-700"
+          onClick={handleToggleComplete}
+        >
+          <FiEdit3 size={20} />
+        </button>
+        <button
+          className="text-red-500 hover:text-red-700"
+          onClick={handleDelete}
+        >
+          <MdDelete size={20} />
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SingleTodo
+export default SingleTodo;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Todo } from '../model';
+import React from "react";
+import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
 
 interface Props {
   todos: Todo[];
@@ -10,21 +11,12 @@ const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
   return (
     <div className="flex flex-wrap gap-4 mt-5 justify-center">
       {todos.map((todo) => (
-        <div
+        <SingleTodo
+          todo={todo}
           key={todo.id}
-          className="bg-white rounded-lg shadow-md p-4 flex flex-col items-center w-1/3 max-w-xs"
-        >
-          <p className="font-semibold text-lg text-gray-800">{todo.name}</p>
-          <button
-            className="mt-2 bg-red-500 text-white py-1 px-3 rounded-lg hover:bg-red-700"
-            onClick={() => {
-              // Remove the todo item when clicked
-              setTodos(todos.filter((t) => t.id !== todo.id));
-            }}
-          >
-            Delete
-          </button>
-        </div>
+          todos={todos}
+          setTodos={setTodos}
+        />
       ))}
     </div>
   );
